29828ede3d723ef6b198e4255edecaf6d6bfcd1d
Azizbek1e6 <edusuz1112@gmail.com>
2025-03-16 06:56:08 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { Routes, Route, useRoutes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import routes from "tempo-routes";
 import Home from "@/components/home";
 import Courses from "@/pages/Courses";
@@ -10,12 +10,9 @@ import Contact from "@/pages/Contact";
 import About from "@/pages/About";
 import Instructors from "@/pages/Instructors";
 import LessonView from "@/pages/LessonView";
+import AppRoutes from "@/routes";
 
 function App() {
-  // Get Tempo routes if in Tempo environment
-  const tempoRoutes =
-    import.meta.env.VITE_TEMPO === "true" ? useRoutes(routes) : null;
-
   return (
     <Suspense
       fallback={
@@ -24,28 +21,10 @@ function App() {
         </div>
       }
     >
-      {/* Render tempo routes */}
-      {tempoRoutes}
+      {/* Use the AppRoutes component which contains all routes */}
+      <AppRoutes />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/course/:id" element={<CourseDetails />} />
-        <Route
-          path="/course/:courseId/lesson/:lessonId"
-          element={<LessonView />}
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/instructors" element={<Instructors />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-
-        {/* Allow tempo to capture routes before catchall */}
-        {import.meta.env.VITE_TEMPO === "true" && (
-          <Route path="/tempobook/*" element={<div />} />
-        )}
-      </Routes>
+      {/* Tempo routes will be handled inside AppRoutes */}
     </Suspense>
   );
 }
